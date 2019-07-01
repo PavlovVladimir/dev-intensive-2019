5f44ea1aabf325fe98fa493d0e199d669c472c4d
1n7 <46773729+1n7@users.noreply.github.com>
2019-07-01 22:00:54 +0300

    humanizediff is doing, mthrfckr

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,7 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
+import ru.skillbranch.devintensive.utils.Utils
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 const val SECOND = 1000L
 const val MINUTE = SECOND*60
@@ -29,7 +31,58 @@ fun Date.add (value:Int, units: TimeUnits = TimeUnits.SECOND):Date {
 
 fun Date.humanizeDiff(date: Date = Date()):String {
 //TODO make IT!
-    return "humanize dont work"
+        val time = date.time
+        val diffSec = if(time>getTime()){(time - getTime())/1000}else{(time-getTime())/1000-1}
+        val diffMin = diffSec/60
+        val diffHour = diffMin/60
+        val diffDay = diffHour/24
+    //println (diffMin)
+
+    if (abs(diffSec.toInt()) in 1..75){
+        if (diffSec in 0..1) return "только что"
+        if (diffSec in 1..45) return "несколько секунд назад"
+        if (diffSec in 45..75) return "минуту назад"
+            //  when {
+            //diffSec > 0 -> return Utils.humaSec(diffSec,"назад","")
+            //diffSec < 0 -> return Utils.humaSec(abs(diffSec),"","через")
+        //}
+    }
+
+    if (abs(diffMin.toInt()) in 1..75){
+        if (diffMin in 46..75) return "час назад"
+        when {
+            diffMin > 0 -> return Utils.humaMin(diffMin," назад","")
+            diffMin < 0 -> return Utils.humaMin(abs(diffMin),"","через ")
+        }
+    }
+
+    if (abs(diffHour.toInt()) in 1..26){
+        if (diffHour in 22..26) return "день назад"
+        if (diffHour in 1..22) {
+
+            when {
+                diffHour > 0 -> return Utils.humaHour(diffHour, " назад", "")
+                diffHour < 0 -> return Utils.humaHour(abs(diffHour), "", "через ")
+            }
+        }
+    }
+
+    if (abs(diffDay)>0){
+        when{
+            diffDay >= 361 -> return "более года назад"
+            diffDay <= -361 -> return "более чем через год"
+            else -> if (diffDay>0){return Utils.humaDay(diffDay," назад","")}else
+            {return Utils.humaDay(abs(diffDay),"","через ")}
+        }
+    }
+
+
+
+
+
+
+
+    return "hahaha"
 }
 
 
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -16,11 +16,90 @@ object Utils {
 
     fun transliteration(payload: String, divider: String = " "): String {
 //TODO make IT!!!
-        return "translit"
+        val str2Translit = payload.toString()
+        var result:String=""
+        val divider = divider
+
+        for (i in str2Translit.indices) result += cyr2lat(str2Translit[i])
+
+        return  result.replace(" ",divider)
+    }
+
+    fun cyr2lat(c:Char?):String{
+        var ch = c.toString()
+        when (c){
+            'а' -> ch="a"
+            'б' -> ch="b"
+            'в' -> ch="v"
+            'г' -> ch="g"
+            'д' -> ch="d"
+            'е' -> ch="e"
+            'ё' -> ch="e"
+            'ж' -> ch="zh"
+            'з' -> ch="z"
+            'и' -> ch="i"
+            'й' -> ch="i"
+            'к' -> ch="k"
+            'л' -> ch="l"
+            'м' -> ch="m"
+            'н' -> ch="n"
+            'о' -> ch="o"
+            'п' -> ch="p"
+            'р' -> ch="r"
+            'с' -> ch="s"
+            'т' -> ch="t"
+            'у' -> ch="u"
+            'ф' -> ch="f"
+            'х' -> ch="h"
+            'ц' -> ch="c"
+            'ч' -> ch="ch"
+            'ш' -> ch="sh"
+            'щ' -> ch="sh"
+            'ъ' -> ch=""
+            'ы' -> ch="i"
+            'ь' -> ch=""
+            'э' -> ch="e"
+            'ю' -> ch="yu"
+            'я' -> ch="ya"
+            'А' -> ch="A"
+            'Б' -> ch="B"
+            'В' -> ch="V"
+            'Г' -> ch="G"
+            'Д' -> ch="D"
+            'Е' -> ch="E"
+            'Ё' -> ch="E"
+            'Ж' -> ch="Zh"
+            'З' -> ch="Z"
+            'И' -> ch="I"
+            'Й' -> ch="I"
+            'К' -> ch="K"
+            'Л' -> ch="L"
+            'М' -> ch="M"
+            'Н' -> ch="N"
+            'О' -> ch="O"
+            'П' -> ch="P"
+            'Р' -> ch="R"
+            'С' -> ch="S"
+            'Т' -> ch="T"
+            'У' -> ch="U"
+            'Ф' -> ch="F"
+            'Х' -> ch="H"
+            'Ц' -> ch="C"
+            'Ч' -> ch="Ch"
+            'Ш' -> ch="Sh"
+            'Щ' -> ch="Sh"
+            'Ъ' -> ch=""
+            'Ы' -> ch="I"
+            'Ь' -> ch=""
+            'Э' -> ch="E"
+            'Ю' -> ch="Yu"
+            'Я' -> ch="Ya"
+        }
+        return ch
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-//TODO make IT!!!
+
         val firstLetter   = firstName?.getOrNull(0)//.toString().toUpperCase()
         val secondLetter  = lastName?.getOrNull(0)//.toString().toUpperCase()
         var initials:String? = null
@@ -43,5 +122,49 @@ object Utils {
         }
 
         return initials
+    }
+
+    fun humaDay(diffT:Long,nazad:String,vpered:String):String{
+        val ends = diffT.toString().takeLast(1)
+
+        if (diffT.toString().takeLast(2) in "11".."14") {return "$vpered$diffT дней$nazad"}
+
+        when (ends){
+            "1" -> return "$vpered$diffT день$nazad"
+            in "2".."4" -> return "$vpered$diffT дня$nazad"
+            else -> return "$vpered$diffT дней$nazad"
+        }
+    }
+
+    fun humaHour(diffT: Long,nazad:String,vpered:String):String{
+
+        val ends = diffT.toString().takeLast(1)
+        if (diffT.toString().takeLast(2) in "11".."14") {return "$vpered$diffT часов$nazad"}
+        when (ends){
+            "1" -> return "$vpered$diffT час$nazad"
+            in "2".."4" -> return "$vpered$diffT часа$nazad"
+            else -> return "$vpered$diffT часов$nazad"
+        }
+    }
+
+    fun humaMin (diffT: Long,nazad:String,vpered:String):String{
+        val ends = diffT.toString().takeLast(1)
+        if (diffT.toString().takeLast(2) in "11".."14") {return "$vpered$diffT минут$nazad"}
+        when (ends){
+            "1" -> return "$vpered$diffT минуту$nazad"
+            in "2".."4" -> return "$vpered$diffT минуты$nazad"
+            else -> return "$vpered$diffT минут$nazad"
+        }
+    }
+
+    fun humaSec (diffT: Long,nazad:String,vpered:String):String{
+        val ends = diffT.toString().takeLast(1)
+       // if (diffT.toString().takeLast(2) in "11".."14") {return "$vpered $diffT секунд $nazad"}
+
+        when (ends){
+            "1" ->  return "$vpered$diffT секунду$nazad"
+            in "2".."4" -> return "$vpered$diffT секунды$nazad"
+            else -> return "$vpered$diffT секунд$nazad"
+        }
     }
 }
