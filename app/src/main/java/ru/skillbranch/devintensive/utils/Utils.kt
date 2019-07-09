10cec8fd770c1971ad95aa629fd3bfd4a548b006
1n7 <46773729+1n7@users.noreply.github.com>
2019-07-09 19:05:59 +0300

    hometask_3 initialize

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,65 +4,65 @@ import ru.skillbranch.devintensive.extensions.TimeUnits
 
 object Utils {
     fun parseFullName (fullName:String?):Pair<String?, String?> {
-        //TODO FIXME!!!
-        val parts: List<String>? = fullName?.split(" ")
 
+        val parts: List<String>? = fullName?.split(" ")
         var firstName = parts?.getOrNull(0)
         var lastName  = parts?.getOrNull(1)
 
-        if (firstName?.length == 0) firstName=null
-        if (lastName?.length == 0)  lastName=null
-//        return Pair(firstName, fullName)
-        return firstName to lastName
+            if (firstName?.length == 0) firstName=null
+            if (lastName?.length == 0)  lastName=null
+
+//           return Pair(firstName, fullName)
+             return firstName to lastName
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-//TODO make IT!!!
+
         val str2Translit = payload.toString()
         var result:String=""
         val divider = divider
 
-        for (i in str2Translit.indices) result += cyr2lat(str2Translit[i])
+            for (i in str2Translit.indices) result += cyr2lat(str2Translit[i])
 
-        return  result.replace(" ",divider)
+                return  result.replace(" ",divider)
     }
 
     fun cyr2lat(c:Char?):String{
         var ch = c.toString()
-        when (c){
-            'а' -> ch="a"
-            'б' -> ch="b"
-            'в' -> ch="v"
-            'г' -> ch="g"
-            'д' -> ch="d"
-            'е' -> ch="e"
-            'ё' -> ch="e"
-            'ж' -> ch="zh"
-            'з' -> ch="z"
-            'и' -> ch="i"
-            'й' -> ch="i"
-            'к' -> ch="k"
-            'л' -> ch="l"
-            'м' -> ch="m"
-            'н' -> ch="n"
-            'о' -> ch="o"
-            'п' -> ch="p"
-            'р' -> ch="r"
-            'с' -> ch="s"
-            'т' -> ch="t"
-            'у' -> ch="u"
-            'ф' -> ch="f"
-            'х' -> ch="h"
-            'ц' -> ch="c"
-            'ч' -> ch="ch"
-            'ш' -> ch="sh"
-            'щ' -> ch="sh"
-            'ъ' -> ch=""
-            'ы' -> ch="i"
-            'ь' -> ch=""
-            'э' -> ch="e"
-            'ю' -> ch="yu"
-            'я' -> ch="ya"
+            when (c){
+                'а' -> ch="a"
+                'б' -> ch="b"
+                'в' -> ch="v"
+                'г' -> ch="g"
+                'д' -> ch="d"
+                'е' -> ch="e"
+                'ё' -> ch="e"
+                'ж' -> ch="zh"
+                'з' -> ch="z"
+                'и' -> ch="i"
+                'й' -> ch="i"
+                'к' -> ch="k"
+                'л' -> ch="l"
+                'м' -> ch="m"
+                'н' -> ch="n"
+                'о' -> ch="o"
+                'п' -> ch="p"
+                'р' -> ch="r"
+                'с' -> ch="s"
+                'т' -> ch="t"
+                'у' -> ch="u"
+                'ф' -> ch="f"
+                'х' -> ch="h"
+                'ц' -> ch="c"
+                'ч' -> ch="ch"
+                'ш' -> ch="sh"
+                'щ' -> ch="sh"
+                'ъ' -> ch=""
+                'ы' -> ch="i"
+                'ь' -> ch=""
+                'э' -> ch="e"
+                'ю' -> ch="yu"
+                'я' -> ch="ya"
             'А' -> ch="A"
             'Б' -> ch="B"
             'В' -> ch="V"
@@ -97,7 +97,7 @@ object Utils {
             'Ю' -> ch="Yu"
             'Я' -> ch="Ya"
         }
-        return ch
+                        return ch
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
@@ -108,25 +108,25 @@ object Utils {
 
 
         when (firstLetter){
-          in 'a'..'z' -> initials = firstLetter.toString().toUpperCase()
+            in 'a'..'z' -> initials = firstLetter.toString().toUpperCase()
             in 'а'..'я' -> initials = firstLetter.toString().toUpperCase()
-          in 'A'..'Z' ->initials = firstLetter.toString()
+            in 'A'..'Z' ->initials = firstLetter.toString()
             in 'А'..'Я' -> initials = firstLetter.toString()
-            else -> {when (secondLetter){
-                in 'a'..'z' -> return secondLetter.toString().toUpperCase()
-                in 'а'..'я' -> return secondLetter.toString().toUpperCase()
-                in 'A'..'Z' -> return secondLetter.toString()
-                in 'А'..'Я' -> return secondLetter.toString()
-                else -> return null
+                else -> {when (secondLetter){
+                    in 'a'..'z' -> return secondLetter.toString().toUpperCase()
+                    in 'а'..'я' -> return secondLetter.toString().toUpperCase()
+                    in 'A'..'Z' -> return secondLetter.toString()
+                    in 'А'..'Я' -> return secondLetter.toString()
+                       else -> return null
             }}
         }
 
         when (secondLetter){
             in 'a'..'z' -> initials = initials + secondLetter.toString().toUpperCase()
             in 'а'..'я' -> initials = initials + secondLetter.toString().toUpperCase()
-            in 'A'..'Z'  -> initials = initials + secondLetter.toString()
+            in 'A'..'Z' -> initials = initials + secondLetter.toString()
             in 'А'..'Я' -> initials = initials + secondLetter.toString()
-            else -> return initials
+            else        -> return initials
         }
 
         return initials
@@ -135,12 +135,12 @@ object Utils {
     fun humaDay(diffT:Long,nazad:String,vpered:String):String{
         val ends = diffT.toString().takeLast(1)
 
-        if (diffT.toString().takeLast(2) in "11".."14") {return "$vpered$diffT дней$nazad"}
+          if (diffT.toString().takeLast(2) in "11".."14") {return "$vpered$diffT дней$nazad"}
 
-        when (ends){
-            "1" -> return "$vpered$diffT день$nazad"
-            in "2".."4" -> return "$vpered$diffT дня$nazad"
-            else -> return "$vpered$diffT дней$nazad"
+            when (ends){
+                "1"         -> return "$vpered$diffT день$nazad"
+                in "2".."4" -> return "$vpered$diffT дня$nazad"
+                   else     -> return "$vpered$diffT дней$nazad"
         }
     }
 
